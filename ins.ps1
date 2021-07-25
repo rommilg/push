161b5ea51224a1fa7d4ce182e2ad4c6e2e48b852
rommilg <romanmilgr@gmail.com>
2021-07-25 04:10:07 +0300

    co

---
+++ ins.ps1
@@ -0,0 +1,6 @@
+npm init -y
+
+npm i web-push
+
+#get vapidKyes
+node push.js
---
+++ push.js
@@ -0,0 +1,21 @@
+var push = require("web-push");
+
+//generate vapidKeys ones
+//let vapidKeys = push.generateVAPIDKeys();
+//console.log(vapidKeys);
+
+let vapidKeys = {
+  publicKey:
+    "BEbMHS7sHTyj7fp3CMFbqfDIoAK6ySmLXvFbcW8o9AX9mJaQAaDKm2c-jbbamddVtELhAFsm3vkt-P3SitDh9FY",
+  privateKey: "U2QivoP7ff1yhsq6adKV18qYiCez8VhJGKjHM-1zN48",
+};
+
+push.setVapidDetails(
+  "mailto:test@gmail.com",
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
+
+let sub{};
+
+push.sendNotification(sub,"te");
